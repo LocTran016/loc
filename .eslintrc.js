fab43229c9fd59e587185f42c0556455732bdee5
LocTran <trungphuc016@Gmail.com>
2021-01-15 20:29:51 +0700

    Update 404.html

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true,
+        "commonjs": true
+    },
+    "plugins": [
+        "html",
+        "css-modules"
+    ],
+    "extends": [
+        "google",
+        "plugin:css-modules/recommended"
+    ],
+    "parserOptions": {
+        "ecmaVersion": 6,
+        "sourceType": "module"
+    },
+    "rules": {
+        "css-modules/no-unused-class": [2, { "camelCase": true }],
+        "css-modules/no-undef-class": [2, { "camelCase": true }]
+    }
+};
