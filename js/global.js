22cf688dcb0a739dd5b45ddee46acbb0ff5c5c17
LocTran <LocTran016@users.noreply.github.com>
2021-01-15 08:24:46 +0700

    Add JS and CSS

---
+++ js/global.js
@@ -0,0 +1,13 @@
+function flashtext(ele, col) {
+    var tmpColCheck = document.getElementById(ele).style.color;
+
+    if (tmpColCheck === 'silver') {
+        document.getElementById(ele).style.color = col;
+    } else {
+        document.getElementById(ele).style.color = 'silver';
+    }
+}
+
+setInterval(function () {
+    flashtext('hello-world', 'red');
+}, 500); //set an interval timer up to repeat the function
