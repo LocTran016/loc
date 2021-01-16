9c637ec4fb4692ce29515d0eb76b41880813b07a
LocTran <trungphuc016@Gmail.com>
2021-01-16 18:40:35 +0700

    Fix gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
-const {gulp, series} = require('gulp');
+const {dest, series, src, watch} = require('gulp');
 const webp = require('gulp-webp');
 const imagemin = require('gulp-imagemin');
 const uglify = require('gulp-uglify');
@@ -8,55 +8,61 @@ const cleanCSS = require('gulp-clean-css');
 var gulpRemoveHtml = require('gulp-remove-html');
 
 function removeCode() {
-    return gulp.src('src/*.html')
+    return src('src/*.html')
       .pipe(gulpRemoveHtml())
-      .pipe(gulp.dest('public/'));
+      .pipe(dest('public/'));
   };
 
 function imgToWeBP() {
-    gulp.src('src/img/')
+    src('src/img/')
         .pipe(webp())
-        .pipe(gulp.dest('public/img/'))
+        .pipe(dest('public/img/'))
 }
 function minImg() {
-    gulp.src('src/img/')
+    src('src/img/')
         .pipe(imagemin())
-        .pipe(gulp.dest('public/img/'))
+        .pipe(dest('public/img/'))
 }
 
 function faviconICO() {
-    gulp.src('src/img/favicon.jpeg')
+    src('src/img/favicon.jpeg')
     .pipe(favicons({
-        manifest,
         headers: true
     }))
-    .pipe(gulp.dest('public/img/'))
+    .pipe(dest('public/img/'))
 }
 function injectAssets() {
-    var target = gulp.src('./src/**/*.html');
+    var target = src('./src/**/*.html');
     // It's not necessary to read the files (will speed up things), we're only after their paths:
-    var sources = gulp.src(['./public/js/*.min.js', './public/css/*.css'], {read: false});
+    var sources = src(['./public/js/*.min.js', './public/css/*.css'], {read: false});
    
     return target.pipe(inject(sources))
-      .pipe(gulp.dest('./public/'));
+      .pipe(dest('./public/'));
   };
 
 function minJS() {
     // Include JavaScript and CSS files in a single pipeline
-    return gulp.src(['src/js/*.js', 'src/css/*.css'])
+    return src('src/js/*.js')
     .pipe(uglify())
     .pipe(rename({ extname: '.min.js' }))
-    .pipe(gulp.dest('public/js/'));
+    .pipe(dest('public/js/'));
   }
 function minCSS(){
-    return gulp.src('src/css/*.css')
+    return src('src/css/*.css')
     .pipe(cleanCSS({debug: true}, (details) => {
         console.log(`${details.name}: ${details.stats.originalSize}`);
         console.log(`${details.name}: ${details.stats.minifiedSize}`);
         console.log(`${details.name}: ${details.errors}`);
         console.log(`${details.name}: ${details.warnings}`);
       }))
-    .pipe(gulp.dest('dist'));
+    .pipe(dest('dist'));
 }
 
-exports.build = series(imgToWeBP, minImg, minCSS, minJS, injectAssets, removeCode, faviconICO);
+function watchTask() {
+    watch(['src/js/*.js', 'src/css/*.css'], series(minJS, minCSS, injectAssets)),
+    watch('./src/**/*.html', removeCode,),
+    watch('src/img/', series(minImg,imgToWeBP))
+    watch('src/img/favicon.jpeg', faviconICO)
+}
+
+exports.default = series(imgToWeBP, minImg, minCSS, minJS, injectAssets, removeCode, faviconICO, watchTask);
