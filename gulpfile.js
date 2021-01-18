6a4a1821bff500a6d672a3e362ff602fabd94ca9
LocTran <trungphuc016@Gmail.com>
2021-01-18 19:30:28 +0700

    Remove trash

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,16 @@ const {dest, series, src, watch} = require('gulp');
 const webp = require('gulp-webp');
 const imagemin = require('gulp-imagemin');
 const favicons = require('@flexis/favicons/lib/stream');
-const inject = require('gulp-inject');
+// const inject = require('gulp-inject');
 const cleanCSS = require('gulp-clean-css');
-const rename = require('gulp-rename');
-const gulpRemoveHtml = require('gulp-remove-html');
+// const rename = require('gulp-rename');
 const terser = require('gulp-terser');
 const eslint = require('gulp-eslint');
 const browserSync = require('browser-sync').create();
+var gulpIf = require('gulp-if');
+var useref = require('gulp-useref');
+var lazypipe = require('lazypipe');
+const sourcemaps = require('gulp-sourcemaps'),
 
 var filePath = {
   src: 'src/',
@@ -20,12 +23,12 @@ var filePath = {
     src: "src/css/*.css",
     dist: "public/css/",
     full_dist: "public/js/*.min.css"
-  }
+  },
   js: {
     src: "src/js/*.js",
     dist: "public/js/",
     full_dist: "public/js/*.min.js"
-  }
+  },
   img: {
     src: 'src/img/*.{jpg,jpeg,png,gif}',
     dist: "public/img/"
@@ -45,6 +48,7 @@ function lint() {
         }))
         // .pipe(eslint.failAfterError());
   }
+
 function imgToWeBP() {
     return src('src/img/')
         .pipe(webp())
@@ -64,43 +68,49 @@ function faviconICO() {
     .pipe(dest('public/img/'))
 }
 
-function html() {
-    var sources = src([filePath.css.full_dist,filePath.js.full_dist], {read: false});
-   
-    return src(filePath.html.src)
-      .pipe(inject(sources))
-      .pipe(dest(filePath.html.dist))
-      src(filePath.html.dist)
-      .pipe(gulpRemoveHtml())
-      .pipe(dest(filePath.html.dist));
-  };
-
-  function js() {
-    return src(filePath.js.src)
-      .pipe(terser({
+function htmlCssJs() {
+    return src(filePath.src)
+      .pipe(useref({}, lazypipe().pipe(sourcemaps.init, { loadMaps: true })))
+      .pipe(gulpIf('*.js',terser({
         module: true,
         ecma: 2015
-      }))
-      .pipe(rename({ extname: '.min.js' }))
-      .pipe(dest(filePath.js.dist));
-  }
-
-function css(){
-    return src(filePath.css.src)
-    .pipe(cleanCSS({debug: true}, (details) => {
+      })))
+      .pipe(gulpIf('*.css',cleanCSS({debug: true}, (details) => {
         console.log(`${details.name}: ${details.stats.originalSize}`);
         console.log(`${details.name}: ${details.stats.minifiedSize}`);
         console.log(`${details.name}: ${details.errors}`);
         console.log(`${details.name}: ${details.warnings}`);
-      }))
-    .pipe(rename({ extname: '.min.css' }))
-    .pipe(dest(filePath.css.dist));
-}
+      })))
+      .pipe(dest(filePath.html.dist));
+  };
+
+//   function js() {
+//     return src(filePath.js.src)
+//       .pipe(terser({
+//         module: true,
+//         ecma: 2015
+//       }))
+//       .pipe(rename({ extname: '.min.js' }))
+//       .pipe(dest(filePath.js.dist));
+//   }
+
+// function css(){
+//     return src(filePath.css.src)
+//     .pipe(cleanCSS({debug: true}, (details) => {
+//         console.log(`${details.name}: ${details.stats.originalSize}`);
+//         console.log(`${details.name}: ${details.stats.minifiedSize}`);
+//         console.log(`${details.name}: ${details.errors}`);
+//         console.log(`${details.name}: ${details.warnings}`);
+//       }))
+//     .pipe(rename({ extname: '.min.css' }))
+//     .pipe(dest(filePath.css.dist));
+// }
 
 function watch() {
   watch(filePath.html.src,browserSync.reload)
-  watch(filePath.js.src, browserSync.reload)
-  watch(filePath.css.src, browserSync.reload)
+  watch(filePath.js.src,browserSync.reload)
+  watch(filePath.css.src,browserSync.reload)
+  watch(filePath.img.src,browserSync.reload)
 }
 
 function liveReload() {
@@ -110,5 +120,6 @@ function liveReload() {
     },
   })
 }
-export.develop = series(liveReload,watch)
-exports.default = series(lint, imgToWeBP, minImg, css, js, html, /*faviconICO,*/);
+exports.favicon = faviconICO()
+exports.develop = series(liveReload,watch)
+exports.default = series(lint, imgToWeBP, minImg, htmlCssJs);
