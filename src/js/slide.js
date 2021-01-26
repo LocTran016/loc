cf1adb5e51a5357ffa8a466a42cabacbf732276d
LocTran <trungphuc016@Gmail.com>
2021-01-26 17:28:28 +0700

    Add misc

---
+++ src/js/slide.js
@@ -0,0 +1,12 @@
+/* eslint-disable linebreak-style */
+Reveal.initialize({
+  hash: true,
+  plugins: [
+    RevealHighlight,
+    RevealNotes,
+    RevealSearch,
+    RevealNotes,
+    RevealMath,
+    RevealZoom,
+  ],
+});
