e5d4e7574999d6701977d0fa669b4e70bc58f80d
LocTran <trungphuc016@Gmail.com>
2021-01-30 14:30:03 +0700

    Update the script

--- src/js/body.js
+++ src/js/body.js
@@ -1,18 +1,21 @@
 /* eslint-disable linebreak-style */
 /* eslint-disable require-jsdoc */
-$('#fullScreenButton')
-    .on('click', () => {
-    if (screenfull.isEnabled) {
-    screenfull.toggle();
-$('#fullscreen').removeAttribute('class');
-$('#fullscreen').setAttribute('class',
+$(document).ready(() => {
+  $('#fullScreenButton')
+      .on('click', () => {
+        if (screenfull.isEnabled) {
+          screenfull.toggle();
+          $('#fullscreen').removeAttribute('class');
+          $('#fullscreen').setAttribute('class',
   screenfull.isFullscreen ? 'bi bi-fullscreen':'bi bi-fullscreen-exit');
-  } else {
-    alert('Your browser doesn\'t support full screen');
-  }
-  if (screenfull.isEnabled) {
-    screenfull.on('change', () => {
-      console.log('Am I fullscreen?',
+        } else {
+          alert('Your browser doesn\'t support full screen');
+        }
+        if (screenfull.isEnabled) {
+          screenfull.on('change', () => {
+            console.log('Am I fullscreen?',
           screenfull.isFullscreen ? 'Yes' : 'No');
-    });
-  }});
+          });
+        }
+      });
+});
--- src/js/cookie-banner.js
+++ src/js/cookie-banner.js
@@ -1,40 +1,40 @@
+/* eslint-disable linebreak-style */
 /*
 * Javascript to show and hide cookie banner using localstroage
 */
 
 /**
- * Shows the Cookie banner 
+ * Shows the Cookie banner
  */
-let showCookieBanner = () => {
-    let cookieBanner = document.getElementsByClassName("nk-cookie-banner")[0];
-    cookieBanner.style.display = "block";
-}
+const showCookieBanner = () => {
+  const cookieBanner = document.getElementsByClassName('nk-cookie-banner')[0];
+  cookieBanner.style.display = 'block';
+};
 
 /**
  * Hides the Cookie banner and saves the value to localstorage
  */
-let hideCookieBanner = () => {
-    localStorage.setItem("web_dev_isCookieAccepted", "yes");
+const hideCookieBanner = () => {
+  localStorage.setItem('web_dev_isCookieAccepted', 'yes');
 
-    let cookieBanner = document.getElementsByClassName("nk-cookie-banner")[0];
-    cookieBanner.style.display = "none";
-}
+  const cookieBanner = document.getElementsByClassName('nk-cookie-banner')[0];
+  cookieBanner.style.display = 'none';
+};
 
 /**
  * Checks the localstorage and shows Cookie banner based on it.
  */
-let initializeCookieBanner = () => {
-    let isCookieAccepted = localStorage.getItem("web_dev_isCookieAccepted");
-    if(isCookieAccepted === null)
-    {
-        localStorage.clear();
-        localStorage.setItem("web_dev_isCookieAccepted", "no");
-        showCookieBanner();
-    }
-    if(isCookieAccepted === "no"){
-        showCookieBanner();
-    }
-}
+const initializeCookieBanner = () => {
+  const isCookieAccepted = localStorage.getItem('web_dev_isCookieAccepted');
+  if (isCookieAccepted === null) {
+    localStorage.clear();
+    localStorage.setItem('web_dev_isCookieAccepted', 'no');
+    showCookieBanner();
+  }
+  if (isCookieAccepted === 'no') {
+    showCookieBanner();
+  }
+};
 
 // Assigning values to window object
 $(document).ready(initializeCookieBanner());
