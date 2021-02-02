52370a30d627f73408b562d2dc1d6e91ca08c5ea
LocTran <trungphuc016@Gmail.com>
2021-02-02 11:41:20 +0700

    Fix bui;d

--- .eleventy.js
+++ .eleventy.js
@@ -1,16 +1,22 @@
 module.exports = function(eleventyConfig) {
-    let markdownIt = require("markdown-it");
-    let markdownItAnchors = require('markdown-it-anchor');
-    let markdownItTOC = require('markdown-it-table-of-contents');
-    let options = {
-      html: true,
-      breaks: true,
-    };
-    var markdownLib = markdownIt(options)
-                                        .use(markdownItAnchors.default)
-                                        .use(markdownItTOC);
-                                        
-    eleventyConfig.setLibrary("md", markdownLib);
+    var markdownIt = require("markdown-it");
+    var markdownItAnchor = require('markdown-it-anchor');
+    var markdownItToc = require('markdown-it-table-of-contents');
+    let mdIt = markdownIt({
+		html: true,
+		breaks: true,
+		linkify: true
+	})
+	.use(markdownItAnchor, {
+		permalink: true,
+		permalinkBefore: false,
+		permalinkClass: "direct-link",
+		permalinkSymbol: "#",
+		level: [1,2,3,4]
+	})
+	.use(markdownItToc);
+
+    eleventyConfig.setLibrary("md", mdIt);
     eleventyConfig.addLayoutAlias('post', 'post.njk');
     eleventyConfig.addLayoutAlias('slide', 'slide.njk');
     return {
