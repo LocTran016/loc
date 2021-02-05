01b2fb6e7f8f1543e1186d85344aab4bb3ecaf78
LocTran <trungphuc016@Gmail.com>
2021-02-05 22:23:03 +0700

    Use require instead

--- algolia.mjs.js
+++ algolia.mjs.js
@@ -1,5 +1,5 @@
 
-import algoliasearch from 'algoliasearch';
+const algoliasearch = require('algoliasearch');
 
 const client = algoliasearch('0IIEX2QW3D', 'a95b28133a4ebdc316bf4cef579dbba1');
 const index = client.initIndex('main');
