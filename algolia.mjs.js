71d73be3a207fd2a2de95c2757169a9758b53613
LocTran <trungphuc016@Gmail.com>
2021-02-05 22:52:35 +0700

    Update algolia.mjs.js

--- algolia.mjs.js
+++ algolia.mjs.js
@@ -4,6 +4,6 @@ const algoliasearch = require('algoliasearch');
 const client = algoliasearch('0IIEX2QW3D', 'a95b28133a4ebdc316bf4cef579dbba1');
 const index = client.initIndex('main');
 
-const records = require('_site/index.json');
+const records = require('public/index.json');
 
 index.saveObjects(records, { autoGenerateObjectIDIfNotExist: true });
