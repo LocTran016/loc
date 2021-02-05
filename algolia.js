9e2456c4e3a55948487a6d04bb5625b86aeeaa6d
LocTran <trungphuc016@Gmail.com>
2021-02-05 22:00:01 +0700

    Add Algolia

--- algolia.js
+++ algolia.js
@@ -3,3 +3,7 @@ import algoliasearch from 'algoliasearch';
 
 const client = algoliasearch('0IIEX2QW3D', 'a95b28133a4ebdc316bf4cef579dbba1');
 const index = client.initIndex('main');
+
+const records = require('./actors.json');
+
+index.saveObjects(records, { autoGenerateObjectIDIfNotExist: true });
