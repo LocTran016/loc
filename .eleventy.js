8d73b3276c6436688f8575fc7e5acb82537a2f64
LocTran <trungphuc016@Gmail.com>
2021-02-07 09:55:03 +0700

    Add some plugins

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,7 @@
 const moment = require('moment');
+const readingTime = require('eleventy-plugin-reading-time');
+const readerBar = require('eleventy-plugin-reader-bar');
+const blogTools = require("eleventy-plugin-blog-tools");
  
 moment.locale('vi');
 module.exports = function(eleventyConfig) {
@@ -42,10 +45,9 @@ module.exports = function(eleventyConfig) {
   });
  
   eleventyConfig.addFilter('dateReadable', date => {
-    return moment(date).utc().format('ll'); // E.g. May 31, 2019
+    return moment(date).utc().format('ll');
   });
-
-  eleventyConfig.addShortcode('excerpt', article => extractExcerpt(article));
+  /*********************** Remove some code for index.json ***************************/
   eleventyConfig.addFilter('algExcerpt', function (text) {
     //first remove code
     text = text.replace(/<code class="language-.*?">.*?<\/code>/sg, '');
@@ -62,6 +64,9 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.setLibrary("md", mdIt);
     eleventyConfig.addLayoutAlias('post', 'post.njk');
     eleventyConfig.addLayoutAlias('slide', 'slide.njk');
+    eleventyConfig.addPlugin(readingTime);
+    eleventyConfig.addPlugin(readerBar);
+    eleventyConfig.addPlugin(blogTools);
     return {
       dir: {
         input: "src",
@@ -69,32 +74,3 @@ module.exports = function(eleventyConfig) {
       }
     };
   };
-
-  // https://keepinguptodate.com/pages/2019/06/creating-blog-with-eleventy/
-  function extractExcerpt(article) {
-    if (!article.hasOwnProperty('templateContent')) {
-      console.warn('Failed to extract excerpt: Document has no property "templateContent".');
-      return null;
-    }
-   
-    let excerpt = null;
-    const content = article.templateContent;
-   
-    // The start and end separators to try and match to extract the excerpt
-    const separatorsList = [
-      { start: '<!-- Excerpt Start -->', end: '<!-- Excerpt End -->' },
-      { start: '<p>', end: '</p>' }
-    ];
-   
-    separatorsList.some(separators => {
-      const startPosition = content.indexOf(separators.start);
-      const endPosition = content.indexOf(separators.end);
-   
-      if (startPosition !== -1 && endPosition !== -1) {
-        excerpt = content.substring(startPosition + separators.start.length, endPosition).trim();
-        return true; // Exit out of array loop on first match
-      }
-    });
-   
-    return excerpt;
-  }
